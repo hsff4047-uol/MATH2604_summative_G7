96e5ea82d6249ddd874302217959b004f7cc26ed
tdvl3720-uol <tdvl3720@leeds.ac.uk>
2026-04-28 11:41:26 +0100

    Update Tridiagonals.java

--- Tridiagonals.java
+++ Tridiagonals.java
@@ -85,7 +85,12 @@ class Tridiagonals {
         }
         return result;
     }
-
+    
+    /**Compute the product between a tridiagonal matrix and a diagonal matrix
+        * @param D a valid diagonal matrix, after comparing it to size of the tridiagonal
+        * @param t a valid tridigonal matrix, after checking it is valid
+        * @return the result after computing the product, or null if it is invalid
+        */
     public static double[][] productWithDiagonal(double[] D, double[][] T) {
         /**Compute the product between a tridiagonal matrix and a diagonal matrix
         * @param D a valid diagonal matrix, after comparing it to size of the tridiagonal
