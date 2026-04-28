99ce53a2bb33721c60b3e69da233e5dacd9929d4
kflj0833-uol <kflj0833@leeds.ac.uk>
2026-04-28 14:35:01 +0100

    Refine documentation and simplify error handling
    no error checks since it works

--- ODE.java
+++ ODE.java
@@ -9,7 +9,7 @@ class ODE {
      * Assumes n > 0.
      * @param a The real constant 'a' used in the equation.
      * @param n How many grid points to use inside the interval.
-     * @return The estimated value at exactly f(0.5). If n is an even number, it averages the two closest points. If the tridiagonal solver fails and gives back null, this just returns Double.NaN to handle the error.
+     * @return The estimated value at f 0.5. If n is an even number, it averages the two points.
      */
     public static double solve(double a, int n) {
         double h = 1.0 / (n + 1);
@@ -19,21 +19,17 @@ class ODE {
         // Building the matrix m and defining vector
         for (int i = 0; i < n; i++) {
             double x = (i + 1) * h;
-           
+            
             M[0][i] = -1.0;
             M[1][i] = 2.0 + (h * h * cos(x));
             M[2][i] = -1.0;
-           
+            
             // v_i is a * x^2, so the RHS is -h^2 * a * x^2
             rhs[i] = -1.0 * (h * h) * a * (x * x);
         }
 
         // use the tridiagonals solver
         double[] w = Tridiagonals.linearSolve(M, rhs);
-
-        if (w == null) {
-            return Double.NaN;
-        }
 
         // if n is odd its on the grid point
         if (n % 2 != 0) {
